9256af571b5a38830e8859e73ba9f23a7afdc17f
PotatoScream <kr.englesson@gmail.com>
2025-01-16 17:09:06 +0000

    changes msg

--- src/java/sample.java
+++ src/java/sample.java
@@ -2,7 +2,7 @@ package guru.qa;
 
 public class Main {
 
-    private static String msg = "Hello qa.guru!";
+    private static String msg = "Hello!";
 
     public static void main(String... args) {
         System.out.println(msg);
